6a8d09108b06ebb5c0b757793c93b2ffe764919f
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-07-16 12:28:22 +0530

    151: Reverse words in a string (Multiple Approaches)

--- Problem 151.java
+++ Problem 151.java
@@ -21,7 +21,7 @@
 // Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
 // You need to reduce multiple spaces between two words to a single space in the reversed string.
 
-
+// BRUTE FORCE
 class Solution {
 	public String reverseWords(String s) {
 		int n = s.length();
@@ -48,4 +48,46 @@ class Solution {
 		}
 		return res;
 	}
+}
+
+
+// IN-BUILT METHODS
+class Solution {
+    public String reverseWords(String s) {
+		// trim leading and trailing spaces
+        s = s.trim();
+		// split string into list of words by 1-space or spaces
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+		// reverse the list
+        Collections.reverse(wordList);
+		// Join List elements by single space and return
+        return String.join(" ",wordList);
+    }
+}
+
+
+// Using ARRAY-DEQUE
+class Solution {
+    public String reverseWords(String s) {
+        int left = 0, right =s.length()-1;
+        while(left<=right && s.charAt(left)== ' ')++left;
+        while(left<=right && s.charAt(right)==' ')--right;
+        
+        Deque<String> d = new ArrayDeque();
+        StringBuilder word = new StringBuilder();
+        
+        while(left<=right){
+            char c = s.charAt(left);
+            if(word.length()!=0 && c == ' '){
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            }
+            else if(c != ' ')
+                    word.append(c);
+            left++;
+        }
+        d.offerFirst(word.toString());
+        
+        return String.join(" ",d);
+    }
 }
